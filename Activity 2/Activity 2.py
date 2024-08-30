cfcd689d41c20d209f5ef9efc5966cd8b0b11e44
Hunya-syete <tinangmarcillones@gmail.com>
2024-08-30 18:14:50 +0800

    Please pass

---
+++ Activity 2/Activity 2.py
@@ -0,0 +1,4 @@
+#  Write a program that will calculate the average grade.
+
+
+# Subjects: Math, English, Science
---
+++ Activity 3/Activity 3.py
@@ -0,0 +1,1 @@
+# Write a program to prompt the user for hours and rate per hour to compute gross pay.
