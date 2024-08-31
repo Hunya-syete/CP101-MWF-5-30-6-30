65d2488a92edcbd181cb5194a305dac45ff4c422
K_2049 <shinkun4004@gmail.com>
2024-08-31 13:03:16 +0800

    Valdez.py

---
+++ Activity 2/Valdez.py
@@ -0,0 +1,10 @@
+math = float(input("math grade: "))
+science = float(input("science grade: "))
+english = float(input("english grade: "))
+
+average = math + science + english
+
+
+average = average/3
+
+print(average)
