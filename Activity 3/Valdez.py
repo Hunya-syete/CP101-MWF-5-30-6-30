c8ea8af3a097d45fc1970771a7872768f3b3cf32
K_2049 <shinkun4004@gmail.com>
2024-08-31 13:04:53 +0800

    Create Valdez.py

---
+++ Activity 3/Valdez.py
@@ -0,0 +1,11 @@
+#User prompt Hour value
+Hours = float(input("Enter Hour value here: "))
+
+#User prompt Rate value
+Rate = float(input("Enter Rate value here: "))
+
+#Calculating Gross Pay
+Pay = Hours * Rate 
+
+#Display Gross Pay value
+print("Pay value is: ",Pay)
