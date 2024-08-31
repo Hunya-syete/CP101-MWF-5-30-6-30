8abac6bebab543ad0f1a7a5593bd3782886e52ef
henriettoledo <henriettoledo@gmail.com>
2024-08-31 16:19:49 +0800

    Create Toledo.py

---
+++ Activity 2/Toledo.py
@@ -0,0 +1,8 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Try programiz.pro")
+math = 95
+science = 80
+english = 92
+average = (math + science + english)/3
+print(average)
