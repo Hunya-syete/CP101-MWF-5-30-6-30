ff6ae4ab82b04913bd66cd65c5f5cd043778b49b
kylejohn06-py <Kingkylegumapit@gmail.com>
2024-09-20 11:38:24 +0800

    Jumalon.py

---
+++ Activity 2/Jumalon.py
@@ -0,0 +1,10 @@
+Math = 95
+English = 98.50
+Science = 90
+
+
+Math = int(input("Math grade: "))
+English = float(input("English grade: "))
+Science = int(input("Science grade: "))
+average = (Math + English + Science)
+print("average grade:", average)
