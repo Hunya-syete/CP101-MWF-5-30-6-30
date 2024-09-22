b89f28b5b44e4ad44f9ba3e99d9d3b003009b21b
Isidrito D. Migriño Jr. <sidmiggy@gmail.com>
2024-09-22 10:30:49 +0800

    Migriño.py

---
+++ Activity 6/Migriño.py
@@ -0,0 +1,12 @@
+name = input("Sid: ")
+age = input("26: ")
+print(f"Hello, {name}! You are {age} years old.")
+
+item = "oranges"
+count = 3
+sentence = "I bought {} {} today.".format(count, item)
+print(sentence)
+
+city = "New Zealand"
+temperature = 15
+print("The temperature in %s is %d degrees Celsius." % (city, temperature))
