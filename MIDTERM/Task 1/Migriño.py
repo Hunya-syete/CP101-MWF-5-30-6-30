719b4c5de8e21001b4798c0aff6a16181cd7a6ba
Isidrito D. Migriño Jr. <sidmiggy@gmail.com>
2024-09-25 21:48:09 +0800

    Migriño.py

--- MIDTERM/Task 1/Migriño.py
+++ MIDTERM/Task 1/Migriño.py
@@ -4,15 +4,15 @@ num2 = float(input("Enter the 2nd number: "))
 
 if operator == "+":
     result = num1 + num2
-    print(round(result, 3))
+    print(result, 3)
 elif operator == "-":
     result = num1 - num2
-    print(round(result, 3))
+    print(result, 3)
 elif operator == "*":
     result = num1 * num2
-    print(round(result, 3))
+    print(result, 3)
 elif operator == "/":
     result = num1 / num2
-    print(round(result, 3))
+    print(result, 3)
 else:
     print(f"{operator} is not a valid operator")
