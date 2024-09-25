b8d3edcaf6eab453c0424a5ab0d3c9658e1fc828
K_2049 <shinkun4004@gmail.com>
2024-09-25 22:10:58 +0800

    Valdez.py

---
+++ MIDTERM/Task 1/Valdez.py
@@ -0,0 +1,16 @@
+# Rustom Carl M. Valdez BSIT (5:30-6:30pm)
+
+num1 = float(input("Enter First number: "))
+num2 = float(input("Enter Second number: "))
+operation = input("Choose operation (+, -, *, /): ")
+
+if operation == "+":
+    print(num1 + num2)
+elif operation == "-":
+    print(num1 - num2)
+elif operation == "*":
+    print(num1 * num2)
+elif operation == "/":
+    print(num1 / num2)
+else:
+    print("AYAY INVALID UYY")
