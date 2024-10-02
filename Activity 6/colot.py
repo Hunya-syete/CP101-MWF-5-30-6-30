3f36f33afdae49e4a58948c67d1e3ae4d1553ccf
crizel2006 <colotcrizel16@gmail.com>
2024-10-02 03:07:07 -0700

    colot.py

---
+++ Activity 6/colot.py
@@ -0,0 +1,12 @@
+name = input ("zel:")
+age = input("18: ")
+print(f"Hello I'm, {name}! You are {age} years old.")
+
+item = "books"
+count = 5
+sentence = "I bring {} {} today.".format(count, item)
+print(sentence)
+
+city = "Dipolog"
+temperature = 29
+print("The temperature in %s is %d degrees Celsius." % (city, temperature))
