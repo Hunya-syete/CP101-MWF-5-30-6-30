3e5064ec8f76e5ef1023d762a718dda0d797c46f
Niko-47 <nikogutib474@gmail.com>
2024-10-24 00:13:59 +0800

    Create Gutib.py

---
+++ Activity 3/Gutib.py
@@ -0,0 +1,9 @@
+# Get input from the user
+hours = float(input("Enter hours worked: "))
+rate_per_hour = float(input("Enter rate per hour: "))
+
+# Calculate gross pay
+gross_pay = hours * rate_per_hour
+
+# Display the result
+print("Gross pay: $", gross_pay)
