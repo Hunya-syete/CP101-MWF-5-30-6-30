b0501fc2f6c4b79cd84e73d403f4dfea3278ed8b
Leogumiason14 <leogumiason24@gmail.com>
2024-12-17 17:11:46 +0800

    Activity_8_Gumiason.py

---
+++ Activity 8/Gumiason.py
@@ -0,0 +1,33 @@
+
+def Guesser():
+  print("Random Number Guessing")
+  print("Guess the Correct Number Between 1 and 20")
+  print("You only have 3 Attempts")
+  
+  Lo = 1
+  Hi = 20
+  Attempts = 0
+  Max_Attempts = 3
+  
+  RN = random.randint(Lo, Hi)
+  
+  while Attempts < Max_Attempts:
+    GN = int(input("What's the number?: "))
+    Attempts += 1
+    
+    if GN > RN:
+      print("lower")
+          
+    elif GN < RN:
+      print("Higher")
+      
+    elif GN == RN:
+      print("Correct")
+      break
+  
+    if Attempts == Max_Attempts:
+        print("Maximum Attempts Reached!")
+        print("Try Again!")
+        break
+ 
+Guesser()
