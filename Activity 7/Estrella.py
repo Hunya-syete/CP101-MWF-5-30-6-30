fd86bb4152f1816532d659ee7f540fb69428b0c7
EstrellaMichaellaMaeM <ronnieemily2317@gmail.com>
2024-12-17 20:37:52 +0800

    Create Estrella.py

---
+++ Activity 7/Estrella.py
@@ -0,0 +1,22 @@
+Course = input("Chose your graduated course(IT, ACC or HM?): ")
+Years = int(input("How many year have you worked?: "))
+
+if Course == "IT":
+    if Years >= 10:
+        Pay = 10000
+    elif Years < 10:
+        Pay = 5000
+        
+if Course == "ACC":
+    if Years >= 10:
+        Pay = 12000
+    elif Years < 10:
+        Pay = 6000
+ 
+if Course == "HM":
+    if Years >= 10:
+        Pay = 15000
+    elif Years < 10:
+        Pay = 7500
+        
+print("Your years of service is cost", Pay)
