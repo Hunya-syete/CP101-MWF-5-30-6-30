15ddcd953dee5e374e1df3e3edc27b3aad2726d0
AG Cambalon <agcambalon@gmail.com>
2024-12-19 22:14:36 +0800

    Cambalon.py

---
+++ Activity 9/Cambalon.py
@@ -0,0 +1,20 @@
+Word_Storage = []
+
+print("Word Bank")
+
+while True:
+    New = input("Enter: ")
+    Word_Storage.append(New)
+      
+    print("Do You Want to Add More?")
+    print("Enter Y(Yes) and N(No)")
+  
+    Ask = input("'Y' or 'N': ").upper()
+    if Ask == 'Y':
+        continue 
+     
+    elif Ask == 'N':
+        Number_of_Words = len(Word_Storage)
+        print("Words: ", Word_Storage)
+        print("Total Number of Words: ", Number_of_Words)
+        break
